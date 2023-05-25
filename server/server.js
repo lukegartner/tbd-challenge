3e8dfbbb8ae64b269674b3ca780baa6ef928ac47
Luke Gartner <lukegartner4@gmail.com>
2023-05-24 19:25:53 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.use(express.static("server"));
+app.use(express.json());
+
+// Get
+app.get("/ping", (req, res) => {
+  res.send("ping back");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on PORT", PORT);
+});
