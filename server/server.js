e08412db9390fad33b551cb4ef52bd3630aaf1f3
Luke Gartner <lukegartner4@gmail.com>
2023-05-24 20:11:32 -0500

    add pg pool and get route for messages

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
+const DB_PASSWORD = process.env(DB_PASSWORD);
+
+const pg = require("pg");
+const pool = new pg.Pool({
+  host: "db.bit.io",
+  port: 5432,
+  ssl: true,
+  database: "lukegartner/tbd",
+  user: "lukegartner",
+  password: DB_PASSWORD,
+});
 
 app.use(express.static("server"));
 app.use(express.json());
@@ -8,6 +19,19 @@ app.use(express.json());
 // Get
 app.get("/ping", (req, res) => {
   res.send("ping back");
+});
+
+app.get("/messages", (req, res) => {
+  let queryText = 'SELECT * FROM "messages"';
+
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 app.listen(PORT, () => {
